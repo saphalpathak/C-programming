8e6984c38f4bbfdf197fbba7c3edc4d7479323aa
Saphal <saphalpathak3@gmail.com>
2020-09-02 11:37:09 +0545

    initial commit

---
+++ add_numbers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a=5, b=7,sum;
+    sum = a+b;
+    printf("First number is %d",a);
+    printf("\nSecond number is %d",b);
+    printf("\nAnd the sum is is %d",sum);
+    printf("\nFirst number is %d, second number is %d and the sum is %d",a,b,sum);
+    printf()
+
+}
---
+++ inputt.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,sum;
+
+    printf("Enter first number: ");
+    scanf("%d",&a);
+    printf("Enter second number: ");
+    scanf("%d",&b);
+    sum = a+b;
+
+    printf("\nFirst number is %d, second number is %d and the sum is %d",a,b,sum);
+
+}
