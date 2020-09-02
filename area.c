73a3c59bd93f458c326edbf7d2d89808ceaa9ff4
Saphal <saphalpathak3@gmail.com>
2020-09-02 11:51:36 +0545

    added area

---
+++ area.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int l,b,area;
+
+    printf("Enter length: ");
+    scanf("%d",&l);
+    printf("Enter breadth: ");
+    scanf("%d",&b);
+    area = l*b;
+
+    printf("Area of rectangle is %d",area);
+
+}
